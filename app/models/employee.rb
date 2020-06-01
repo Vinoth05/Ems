e66333854db1e5d3ed2e309659be0aecda1c666c
vinoth <vinoth.mohandoss@tnqsoftware.co.in>
2020-06-01 12:30:24 +0530

    added validations for the employee add and edit form

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,5 @@
 class Employee < ApplicationRecord
+    validates :name, :age, :gender, :designation, presence: true
+    validates :age, numericality: {message: "should be a number"},
+    :unless => Proc.new {|data| data.age.blank?}
 end
